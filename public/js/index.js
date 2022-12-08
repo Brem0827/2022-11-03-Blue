87762bb7beb1844cc2abea50524187959206eabd
Brem0827 <62270266+Brem0827@users.noreply.github.com>
2022-12-08 11:34:34 +0900

    fix

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,17 @@
+const headeruserNameElement = document.querySelector(',user');
 const userNameElement = document.querySelector('.user-name');
+const localuserName = localStorage.getItem('userName');
+
+if (localuserName) {
+  headeruserNameElement.innerHTML = `${localuserName}<span>님</span>`;
+  userNameElement.innerHTML = `${localuserName}<span>님</span>`;
+}
 
 userNameElement.innerHTML = `${localStorage.getItem('userName')}<span>님</span>`;
 
 userNameElement.onclick = () => {
   const userName = prompt('이름을 입력해 주세요');
   localStorage.setItem('userName', userName); // 새로고침을 해도 값이 유지됨
+
   userNameElement.innerHTML = `${userName}<span>님</span>`;
 };
