e38f83b3754257161d50d429347b2397168c4266
Brem0827 <62270266+Brem0827@users.noreply.github.com>
2022-12-08 11:30:07 +0900

    fix

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ const userNameElement = document.querySelector('.user-name');
 userNameElement.innerHTML = `${localStorage.getItem('userName')}<span>님</span>`;
 
 userNameElement.onclick = () => {
-  const userName = confirm('이름을 입력해 주세요');
+  const userName = prompt('이름을 입력해 주세요');
   localStorage.setItem('userName', userName); // 새로고침을 해도 값이 유지됨
   userNameElement.innerHTML = `${userName}<span>님</span>`;
 };
