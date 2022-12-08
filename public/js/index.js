0ef71d53e8473b0c276403c6c3456462f0a94e9d
Brem0827 <62270266+Brem0827@users.noreply.github.com>
2022-12-08 12:23:03 +0900

    add

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,39 @@
 const headeruserNameElement = document.querySelector('.user');
+const headerUserIDElement = document.querySelector('.user');
+const headerUserEmailElement = document.querySelector('.user');
 const userNameElement = document.querySelector('.user-name');
 const localuserName = localStorage.getItem('userName');
 const userNameBtn = document.querySelector('#name');
+const userEmailBtn = document.querySelector('#email');
+const userIDBtn = document.querySelector('#StudentID');
+
+const userEmailElement = document.querySelector('.user-email');
+const localuserEmail = localStorage.getItem('userEmail');
 
 const setUserNameInnerHtml = (name) => {
   headeruserNameElement.innerHTML = `${name}<span>님</span>`;
   userNameBtn.textContent = name;
 };
 
+const setUserIDInnerHtml = (ID) => {
+  headerUserIDElement.innerHTML = `${ID}<span>님</span>`;
+  userIDBtn.textContent = ID;
+};
+
+const setUserEmailInnerHtml = (Email) => {
+  headerUserEmailElement.innerHTML = `${Email}<span>님</span>`;
+  userEmailBtn.textContent = Email;
+};
+
+
 if (localuserName) {
   setUserNameInnerHtml(localuserName);
+}
+if (localuserEmail) {
+  setUserIDInnerHtml(localuserEmail);
+}
+if (localuserEmail) {
+  setUserEmailInnerHtml(localuserEmail);
 }
 
 // eslint-disable-next-line max-len
@@ -20,4 +44,18 @@ userNameElement.onclick = () => {
   localStorage.setItem('userName', userName); // 새로고침을 해도 값이 유지됨
 
   setUserNameInnerHtml(userName);
+};
+
+userEmailElement.onclick = () => {
+  const userEmail = prompt('아이디를 입력해 주세요');
+  localStorage.setItem('userName', userEmail); // 새로고침을 해도 값이 유지됨
+
+  setUserEmailInnerHtml(userEmail);
+};
+
+userEmailElement.onclick = () => {
+  const userID = prompt('이름을 입력해 주세요');
+  localStorage.setItem('userName', userID); // 새로고침을 해도 값이 유지됨
+
+  setUserIDInnerHtml(userID);
 };
