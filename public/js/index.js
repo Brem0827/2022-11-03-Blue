3cc58e53d9f98af9a5942bc364728315817c0ec9
Brem0827 <62270266+Brem0827@users.noreply.github.com>
2022-12-08 11:42:58 +0900

    fix

--- public/js/index.js
+++ public/js/index.js
@@ -18,5 +18,5 @@ userNameElement.onclick = () => {
   const userName = prompt('이름을 입력해 주세요');
   localStorage.setItem('userName', userName); // 새로고침을 해도 값이 유지됨
 
-  setUserNameInnerHtml(localuserName);
+  setUserNameInnerHtml(userName);
 };
